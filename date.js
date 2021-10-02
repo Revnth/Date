3b2b85511f1150f746a9ae70b73e736b50ec514c
Yash Shendge <75063323+Yash-Hax@users.noreply.github.com>
2021-10-02 15:58:17 +0530

    Create date.js

---
+++ date.js
@@ -0,0 +1,10 @@
+Finding date with code 
+
+
+<script>
+var date=new Date();
+var day=date.getDate(); //use getimte method
+var month=date.getMonth()+1;
+var year=date.getFullYear();
+document.write("<br>Date is: "+day+"/"+month+"/"+year);
+</script>
